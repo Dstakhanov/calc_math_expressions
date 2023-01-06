6c9c8565f73ad90b66880634a66b8fde57c83f5f
Dmitriy <dimitrium2011@gmail.ru>
2023-01-06 20:57:31 +0300

    Initial commit

---
+++ bin/calc_math_expressions.dart
@@ -0,0 +1,5 @@
+import 'package:calc_math_expressions/calc_math_expressions.dart' as calc_math_expressions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calc_math_expressions.calculate()}!');
+}
